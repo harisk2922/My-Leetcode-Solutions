2da08e4f9f3387cab1bf9f38dedb6fbadab74599
Haris Khan <harisk2922@gmail.com>
2024-09-23 14:39:26 +0500

    Middle of the Linked List

---
+++ MiddleOfTheLinkedList.js
@@ -0,0 +1,16 @@
+var middleNode = function(head) {
+    length = 0;
+    current = head;
+    while(current!==null) {
+        length++;
+        current = current.next;
+    }
+    let middle = Math.floor(length / 2);
+    current = head;
+    let i = 0
+    while(i!==middle) {
+        current = current.next;
+        i++;
+    }
+    return current;
+};
