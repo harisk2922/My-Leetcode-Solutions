014d6efd5f4d896a3cc11773334b22ced742d911
Haris Khan <harisk2922@gmail.com>
2024-09-25 21:26:38 +0500

    30 Days Of JavaScript - 10

---
+++ 30 Days Of JavaScript/AllowOneFunctionCall.js
@@ -0,0 +1,18 @@
+var once = function (fn) {
+    let called = false;
+    let result;
+    return function (...args) {
+        if (!called) {
+            called = true;
+            result = fn(...args);  
+            return result;
+        } else {
+            return undefined; 
+        }
+    };
+};
+
+let fn = (a,b,c) => (a + b + c)
+let onceFn = once(fn)
+console.log(onceFn(1,2,3)); // 6
+console.log(onceFn(2,3,6)); // returns undefined without calling fn
