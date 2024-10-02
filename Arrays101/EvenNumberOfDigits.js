e0f92a559d4ee1f6a31af6b4ba27f5875b76b762
Haris Khan <harisk2922@gmail.com>
2024-10-02 20:06:06 +0500

    Arrays101 - 2

---
+++ Arrays101/EvenNumberOfDigits.js
@@ -0,0 +1,18 @@
+var findNumbers = function(nums) {
+    let howMany = 0;
+    for (let i = 0; i < nums.length; i++) {
+        let count = 0;
+        let num = nums[i];
+        while (num > 0) {
+            num = Math.floor(num / 10);
+            count++;
+        }
+        if (count % 2 === 0) {
+            howMany++;
+        }
+    }
+    return howMany;
+};
+
+var nums = [5820,317,640,957,7182,764];
+console.log(findNumbers(nums));
