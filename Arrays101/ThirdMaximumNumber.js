eceec2ec61d3187dc292efbe54725cda19e1ef40
Haris Khan <harisk2922@gmail.com>
2024-10-08 15:11:51 +0500

    Arrays101 - 14

---
+++ Arrays101/ThirdMaximumNumber.js
@@ -0,0 +1,18 @@
+var thirdMax = function(nums) {
+    let newnums = Array.from(new Set(nums));
+    newnums.sort((a,b) => b-a);
+    if(newnums.length<3) {
+        return newnums[0];
+    } else {
+        return newnums[2];
+    }
+};
+
+var nums = [3, 2, 1];
+console.log(thirdMax(nums));  
+
+nums = [1, 2];
+console.log(thirdMax(nums)); 
+
+nums = [2, 2, 3, 1];
+console.log(thirdMax(nums)); 
