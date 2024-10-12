710dafd1a87a56f43ba1c6c148db3d809e6cb846
Haris Khan <harisk2922@gmail.com>
2024-10-12 17:23:06 +0500

    Array And String - 4

---
+++ Array And String/2D Array/DiagonalTraverse.js
@@ -0,0 +1,47 @@
+function findDiagonalOrder(mat) {
+    if (mat.length === 0 || mat[0].length === 0) return [];
+
+    const m = mat.length;
+    const n = mat[0].length;
+    let result = [];
+    let i = 0, j = 0;
+    let goingUp = true;
+
+    while (result.length < m * n) {
+        result.push(mat[i][j]);
+
+        if (goingUp) {
+            if (j === n - 1) {
+                i++;
+                goingUp = false;
+            } else if (i === 0) {
+                j++;
+                goingUp = false;
+            } else {
+                i--;
+                j++;
+            }
+        } else {
+            if (i === m - 1) {
+                j++;
+                goingUp = true;
+            } else if (j === 0) {
+                i++;
+                goingUp = true;
+            } else {
+                i++;
+                j--;
+            }
+        }
+    }
+
+    return result;
+}
+
+let mat = [
+            [1,2,3],
+            [4,5,6],
+            [7,8,9]
+];
+
+console.log(findDiagonalOrder(mat));
