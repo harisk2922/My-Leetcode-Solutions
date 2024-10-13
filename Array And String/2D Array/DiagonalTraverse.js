9c0a83f72eed3c3b0fa52bb4b5e00deb59000c4d
Haris Khan <harisk2922@gmail.com>
2024-10-13 23:52:33 +0500

    Array And String - 4

--- Array And String/2D Array/DiagonalTraverse.js
+++ Array And String/2D Array/DiagonalTraverse.js
@@ -41,7 +41,7 @@ function findDiagonalOrder(mat) {
 let matrix = [
             [1,2,3],
             [4,5,6],
-            [7,8,9]
+            [7,8,9]  
     ];
 
 console.log(findDiagonalOrder(matrix));
